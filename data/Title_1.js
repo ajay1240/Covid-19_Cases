af26210bf7f5c9ed1c6409a17b9cd703d9ef0f34
ajay1240 <66860958+ajay1240@users.noreply.github.com>
2020-06-13 18:17:24 +0545

    Add files via upload

---
+++ data/Title_1.js
@@ -0,0 +1,9 @@
+var json_Title_1 = {
+"type": "FeatureCollection",
+"name": "Title_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Designer": "Designed By: Ajay Yadav" }, "geometry": { "type": "Point", "coordinates": [ 79.932871870593274, 25.915563856441249 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Designer": "COVID-19 Outbreak in Nepal" }, "geometry": { "type": "Point", "coordinates": [ 82.542808415311214, 30.826551657304133 ] } }
+]
+}
---
+++ data/TotalCases_2.js
@@ -0,0 +1,12 @@
+var json_TotalCases_2 = {
+"type": "FeatureCollection",
+"name": "TotalCases_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "cases": "Nepal: Total" }, "geometry": { "type": "Point", "coordinates": [ 86.189996342287699, 30.425811477930136 ] } },
+{ "type": "Feature", "properties": { "id": "2", "cases": "Conformed: 5062" }, "geometry": { "type": "Point", "coordinates": [ 86.189996342287643, 30.154906490525669 ] } },
+{ "type": "Feature", "properties": { "id": "3", "cases": "Active: 4171" }, "geometry": { "type": "Point", "coordinates": [ 86.189996342287657, 29.832237968718651 ] } },
+{ "type": "Feature", "properties": { "id": "4", "cases": "Deaths: 16" }, "geometry": { "type": "Point", "coordinates": [ 86.170387805045863, 29.542648200369864 ] } },
+{ "type": "Feature", "properties": { "id": "5", "cases": "Recovered: 875" }, "geometry": { "type": "Point", "coordinates": [ 86.189996342287657, 29.183771636426815 ] } }
+]
+}
